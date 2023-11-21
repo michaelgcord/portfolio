68511d93bb74e7dff9e4b1d6c93327db6ec7257f
Michael <michaelgcord@gmail.com>
2023-11-21 12:32:06 -0800

    enable static export

--- next.config.js
+++ next.config.js
@@ -6,13 +6,13 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
     // /**
     //  * Enable static exports for the App Router.
     //  *
     //  * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
     //  */
-    // output: "export",
+    output: "export",
   
     // /**
     //  * Set base path. This is usually the slug of your repository.
